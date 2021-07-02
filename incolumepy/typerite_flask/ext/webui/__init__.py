09919794d948c09f35f6cbc41066f465970b150b
Britodfbr <britodfbr@gmail.com>
2021-07-02 16:37:45 -0300

    Configuração do blueprint com application factory para webui

--- incolumepy/typerite_flask/ext/webui/__init__.py
+++ incolumepy/typerite_flask/ext/webui/__init__.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from .main import bp
+
 __author__ = '@britodfbr'
+
+
+def init_app(app):
+    app.register_blueprint(bp)
--- incolumepy/typerite_flask/main.py
+++ incolumepy/typerite_flask/main.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from incolumepy.typerite_flask.ext.webui.main import bp
+from incolumepy.typerite_flask.ext import webui
 
 
-def create_app():
+def minimal_app():
     app = Flask(__name__)
-    app.register_blueprint(bp)
+    # app.register_blueprint(bp)
 
     @app.route("/hello")
     def hello():
         return "<p>Hello, World!</p>"
+    return app
+
+
+def create_app():
+    app = minimal_app()
+    webui.init_app(app)
     return app
